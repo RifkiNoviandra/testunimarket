1bca6e4271b8900fbfcaddce359659053ef654de
RifkiNoviandra <Rnoviandra3@gmail.com>
2024-12-29 17:46:36 +0700

    update

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -25,4 +25,38 @@ class ReviewController extends Controller
         }
         return redirect()->route('product.detail', $id)->with('message');
     }
+
+    function update(Request $request, $id){
+
+        $request->validate([
+            'review' => 'required',
+        ]);
+
+        $data = Review::find($id);
+
+        $input = $request->only('review');
+
+        if ($request->rating) {
+            $input['rating'] = $request->rating;
+        }
+
+        $update = $data->update($input);
+
+        if (!$update) {
+            return redirect()->route('product.detail', $data['product_id'])->with('message');
+        }
+        return redirect()->route('product.detail', $data['product_id'])->with('message');
+    }
+
+    function delete(Request $request, $id){
+
+        $data = Review::find($id);
+
+        $delete = $data->delete();
+
+        if (!$delete) {
+            return redirect()->route('product.detail', $data['product_id'])->with('message');
+        }
+        return redirect()->route('product.detail', $data['product_id'])->with('message');
+    }
 }
